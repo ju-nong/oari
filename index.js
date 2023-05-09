8f13506301476b0784170ad2b558ea19b3d30cd0
이준용 <iiiuooyoo@gmail.com>
2023-05-09 15:38:04 +0900

    v0.3.3

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-// const fs = require("fs");
+const LENGTH = 29;
 
-async function getAri() {
-    // const path = `/node_modules/oari/images`;
-    // const dir = fs.readdirSync(path);
-
-    // return dir.length;
+async function get() {
+    const name = `${Math.floor(Math.random() * LENGTH) + 1}.jpg`;
+    const path = `/node_modules/oari/images/${name}`;
 
     try {
-        const response = await fetch(filePath);
+        const response = await fetch(path);
         if (response.status === 200) {
             const blob = await response.blob();
             return {
-                filepath: filePath,
-                filename: imageFileName,
+                filepath: path,
+                filename: name,
                 file: blob,
             };
         } else {
-            console.error(`There is no such Ari${filePath}`);
+            console.error(`There is no such Ari: ${name}`);
             return null;
         }
     } catch (error) {
-        console.error(`Error fetching Ari${filePath}`);
+        console.error(`Error fetching Ari: ${name}`);
         return null;
     }
 }
 
-// exports = getAri;
-module.exports = getAri;
+module.exports = get;
