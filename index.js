1abe7715c3a6544d035666758bd171c598dfc648
이준용 <iiiuooyoo@gmail.com>
2023-05-09 14:44:43 +0900

    v0.2.5
    blob으로 가져와짐

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const getAri = async (number) => {
-    const imageFileName = `${number}.jpg`;
-    const filePath = require(`/node_modules/oari/images/${imageFileName}`);
+// const fs = require("fs");
+
+async function getAri() {
+    // const path = `/node_modules/oari/images`;
+    // const dir = fs.readdirSync(path);
+
+    // return dir.length;
 
     try {
         const response = await fetch(filePath);
         if (response.status === 200) {
             const blob = await response.blob();
-            return { [imageFileName]: blob };
+            return {
+                filepath: filePath,
+                filename: imageFileName,
+                file: blob,
+            };
         } else {
             console.error(`There is no such Ari${filePath}`);
-            return {};
+            return null;
         }
     } catch (error) {
         console.error(`Error fetching Ari${filePath}`);
-        return {};
+        return null;
     }
-};
+}
 
+// exports = getAri;
 module.exports = getAri;
